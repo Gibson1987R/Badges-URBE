76ff0ed5e82170b0e5c895d82dc71d693efcfb0e
Gibson Rosales <gibsonrosales@gmail.com>
2020-07-31 16:13:51 -0400

    Badge terminado, previo inicio a la creación de la página contenedora y creadora de dges

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import './global.css';
-import App from './components/App';
+import './global.css'
+import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<App />, container);
+ReactDOM.render(<Badge firstName='Many' lastName='Fuenmayor' jobTitle='Profesor y Administrador' twitter='RosalesGibson' avatarUrl='https://s.gravatar.com/avatar/dd18a220fc4d5d1191bf6432435c4d3f?s=80'/>, container);
