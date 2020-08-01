10bbfe03ee95a8251837cb6ae509340682746640
Gibson Rosales <gibsonrosales@gmail.com>
2020-08-01 18:21:34 -0400

    Revisado y enlazado de enventos en trePágina(BadgesNew) y formulario creado

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -2,10 +2,28 @@ import React from 'react';
 
 import './styles/BadgeNew.css'
 import URBE2 from '../images/Logo-URBE2.png';
-import Navbar from '../components/Navbar'
-import Badge from '../components/Badge'
+import Navbar from '../components/Navbar';
+import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
 
 class BadgeNew extends React.Component{
+  state = { form:{
+    firstName: '',
+    lastName: '',
+    email: '',
+    jobTitle: '',
+    twitter: '',
+  }};
+
+  handleChange = e => {  
+    this.setState ({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+  
   render(){
     return(
       <div>
@@ -15,8 +33,20 @@ class BadgeNew extends React.Component{
         </div>
         <div className='container'>
           <div className= 'row'>
-            <div className = 'col'>
-              <Badge firstName='Gibson' lastName='Rosales' twiter='RosalesGibson' jobTitle='Profesor y Administrador' avatarUrl='https://s.gravatar.com/avatar/dd18a220fc4d5d1191bf6432435c4d3f?s=80'/>
+            <div className = 'col-6'>
+              <Badge 
+              firstName={this.state.form.firstName} 
+              lastName={this.state.form.lastName} 
+              twitter={this.state.form.twitter} 
+              jobTitle={this.state.form.jobTitle}
+              email={this.state.form.email} 
+              avatarUrl='https://s.gravatar.com/avatar/dd18a220fc4d5d1191bf6432435c4d3f?s=80'/>
+            </div>
+            <div className= 'col-6'>
+              <BadgeForm 
+              onChange={this.handleChange}
+              formValues ={this.state.form}
+              />
             </div>
           </div>
 
